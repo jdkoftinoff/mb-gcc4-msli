f41387bd7e4dcc2a6ebf10e282d146bf34a8c71e
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2010-12-24 14:10:38 -0800

    build_all.sh builds everything, creates prepare-mb-gcc4.sh script to setup path

---
+++ mb_gnu/build_all.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -e
+
+export TOP="$PWD"
+
+if [ ! -f "$TOP"/prepare-mb-gcc4.sh ]; then
+    cd "$TOP"/mb_gnu
+    ./build_binutils.sh
+    ./build_elf2flt.sh
+    ./build_gcc.sh
+    ./build_gdb.sh
+    ./build_genromfs.sh
+    echo "PATH=\"$TOP/mb-gcc4-msli/mb_gnu/release/lin64/bin:$TOP/mb-gcc4-msli/mb_gnu/release/lin/bin:\$PATH\"" >"$TOP"/prepare-mb-gcc4.sh
+fi
